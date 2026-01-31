c6846c75b1fe4a790ec93c9db7ebd1427e17685d
Parthav Shah <parthavshah@Parthavs-MacBook-Air.local>
2026-01-31 21:09:59 +0530

    Dark theme age counter with months and precise logic

--- src/components/CelebrationPage.jsx
+++ src/components/CelebrationPage.jsx
@@ -389,7 +389,7 @@ function CelebrationPage({ onComplete, musicPlayerRef }) {
                   data-button="message"
                   onClick={() => handleButtonClick("message")}
                 >
-                  💌 Well, I Have a Message for You Madam Ji
+                  Oye i wanna tell you something
                 </button>
               )}
             </div>
